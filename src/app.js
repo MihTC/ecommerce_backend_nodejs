bea2c41202f58a8f8f9fcd6e75617fcf7e6dabea
MihTC <minhtran.kgka@gmail.com>
2024-05-15 23:03:12 +0700

    add morgan, helmet, compression package

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
-const express = require(`express`)
+const compression = require('compression')
+const express = require('express')
+const { default: helmet } = require('helmet')
+const morgan = require("morgan")
 const app = express()
 
+
 // init middlewares
+app.use(morgan('dev'))
+app.use(helmet())
+app.use(compression())
 
 // init db
 
 // init routers
+app.get('/', (req, res, next) => {
+    const strCompress = 'Hello CM'
 
+    return res.status(200).json({
+        message: 'Welcome CongMinh',
+        metadata: strCompress.repeat(100000)
+    })
+})
 // handling error
 module.exports = app
