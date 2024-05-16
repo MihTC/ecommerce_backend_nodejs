89c5db6b643e3ba37a71d1085fe5a74f1e619bfa
MihTC <minhtran.kgka@gmail.com>
2024-05-17 00:56:46 +0700

    connect Mongodb

--- src/app.js
+++ src/app.js
@@ -1,25 +1,26 @@
-const compression = require('compression')
-const express = require('express')
-const { default: helmet } = require('helmet')
-const morgan = require("morgan")
-const app = express()
-
+const compression = require("compression");
+const express = require("express");
+const { default: helmet } = require("helmet");
+const morgan = require("morgan");
+const app = express();
 
 // init middlewares
-app.use(morgan('dev'))
-app.use(helmet())
-app.use(compression())
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(compression());
 
 // init db
-
+require("./dbs/init.mongodb");
+const { checkOverLoad } = require("./helpers/check.connect");
+checkOverLoad();
 // init routers
-app.get('/', (req, res, next) => {
-    const strCompress = 'Hello CM'
+app.get("/", (req, res, next) => {
+  const strCompress = "Hello CM";
 
-    return res.status(200).json({
-        message: 'Welcome CongMinh',
-        metadata: strCompress.repeat(100000)
-    })
-})
+  return res.status(200).json({
+    message: "Welcome CongMinh",
+    metadata: strCompress.repeat(100000),
+  });
+});
 // handling error
-module.exports = app
+module.exports = app;
