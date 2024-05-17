f630a79160613b631ad9c4da7cd6b3e648c09928
MihTC <minhtran.kgka@gmail.com>
2024-05-17 22:42:30 +0700

    config

--- src/dbs/init.mongodb.js
+++ src/dbs/init.mongodb.js
@@ -1,9 +1,15 @@
 "use strict";
 
 const mongoose = require("mongoose");
+const {
+  db: { host, name, port },
+} = require("../configs/config.mongodb");
 
-const connectString = `mongodb://localhost:27017/TCMshop`;
+const connectString = `mongodb://${host}:${port}/${name}`;
 const { countConnect } = require("../helpers/check.connect");
+
+console.log("connectString: ", connectString);
+
 class Database {
   constructor() {
     this.connect();
