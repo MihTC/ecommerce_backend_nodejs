fe61a4ab484f0ab65f57c480fb6716adc474a93e
MihTC <minhtran.kgka@gmail.com>
2024-05-28 20:01:16 +0700

    add SignUp and create tokens

--- src/app.js
+++ src/app.js
@@ -9,19 +9,18 @@ const app = express();
 app.use(morgan("dev"));
 app.use(helmet());
 app.use(compression());
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
 // init db
 require("./dbs/init.mongodb");
 const { checkOverLoad } = require("./helpers/check.connect");
 checkOverLoad();
 // init routers
-app.get("/", (req, res, next) => {
-  const strCompress = "Hello CM";
-
-  return res.status(200).json({
-    message: "Welcome CongMinh",
-    metadata: strCompress.repeat(100000),
-  });
-});
+app.use("/", require("./routers"));
 // handling error
 module.exports = app;
